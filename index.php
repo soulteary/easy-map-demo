a9d434504fdf7b0527ca333732d2b76f50d8084e
soulteary <soulteary@qq.com>
2013-05-15 14:08:46 +0800

    更新首页以及使用BT

--- index.php
+++ index.php
@@ -1,100 +1,112 @@
-<!doctype html>
-<html lang="zh-CN">
+<!DOCTYPE html>
+<html lang="zh_CN">
 <head>
-    <meta charset="UTF-8">
-    <title>Story - 校园相册</title>
-    <link rel="stylesheet" href="./extra/index/style.css"/>
+    <meta charset="utf-8">
+    <title>智能公交查询系统</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Le styles -->
+    <link rel="stylesheet" href="./extra/bootstrap.min.css"/>
+    <link rel="stylesheet" href="./extra/main.less"/>
+    <link rel="stylesheet" href="./extra/bootstrap-responsive.min.css"/>
 </head>
-<body onload="PageLoaded();">
-<div id="loader">
-    <p>页面正在加载中 ...</p>
+<body>
 
-    <div id="progress">
-        <div class="warp">
-            <div id="finish"></div>
+<div class="container">
+
+    <div class="masthead">
+        <h3 class="muted">智能公交查询系统</h3>
+        <div class="navbar">
+            <div class="navbar-inner">
+                <div class="container">
+                    <ul class="nav">
+                        <li class="active"><a href="#CMD:HOME">首页</a></li>
+                        <li><a href="#:FAV">收藏</a></li>
+                        <li><a href="#:ABOUT">关于</a></li>
+                        <li><a href="#:HELP">帮助</a></li>
+                        <li><a href="#CONTACT">联系</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div><!-- /.navbar -->
+    </div>
+
+    <div class="jumbotron">
+        <h1>掌握实时出行数据!</h1>
+        <p class="lead">改变乘客对公交运行状况毫不知情的状况，让乘客在赶往车站途
+            中即可根据自身实际情况，获取路况和车辆的实时运行情况以制定更为人性化的乘车
+            计划。</p>
+        <a class="btn btn-large btn-success" href="#CMD:BEGIN">开始使用</a>
+    </div>
+    <hr>
+
+    <div class="row-fluid">
+        <div class="bs-docs-example form-search">
+            <div class="input-append span11 offset1">
+                <input class="span10 search-query" type="text" placeholder="请输入路线或者公交线路">
+                <a href="#CMD:SEARCH">搜索!</a>
+            </div>
         </div>
     </div>
-</div>
+    <hr>
 
-<div class="media-box">
-    <object id="movie" classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000"
-            codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,24" width="780"
-            height="381">
-        <param name="movie" value="extra/index/flash.swf"/>
-        <param name="quality" value="high"/>
-        <param name="menu" value="false"/>
-        <param name="wmode" value="transparent"/>
-        <object data="extra/index/flash.swf" width="780" height="381" type="application/x-shockwave-flash">
-            <param name="quality" value="high"/>
-            <param name="menu" value="false"/>
-            <param name="wmode" value="transparent"/>
-            <param name="pluginurl" value="http://www.macromedia.com/go/getflashplayer"/>
-            MAYBE YOU SHOULD MAIL ME,SOULTEARY@QQ.COM.
-        </object>
-    </object>
-</div>
+    <div class="row-fluid">
+        <div class="span3">
+            <div class="table-header">
+                <span class="bus-no badge badge-important pull-left">811路</span>
+                <span class="label label-info pull-right">约30分钟 / 4.3公里</span>
+            </div>
+            <table class="table table-striped route-table">
+                <thead>
+                <tr>
+                    <th>#</th>
+                    <th>路线引导</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td><span class="badge">1</span></td>
+                    <td><span class="route-info">雄楚国际大酒店</span></td>
+                </tr>
+                <tr>
+                    <td><span class="badge">2</span></td>
+                    <td><span class="route-info">70米</span></td>
+                </tr>
+                <tr>
+                    <td><span class="badge">3</span></td>
+                    <td><span class="route-info">步行至 雄楚大道元宝山站</span></td>
+                </tr>
+                <tr>
+                    <td><span class="badge">4</span></td>
+                    <td><span class="route-info">8站</span></td>
+                </tr>
+                <tr>
+                    <td><span class="badge">5</span></td>
+                    <td><span class="route-info">乘坐 811路(或 586路, 590路), 在 民族大道时间广场站 下车</span></td>
+                </tr>
+                <tr>
+                    <td><span class="badge">6</span></td>
+                    <td><span class="route-info">10米</span></td>
+                </tr>
+                <tr>
+                    <td><span class="badge">7</span></td>
+                    <td><span class="route-info">步行至 中南民族大学</span></td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="span9">
 
-<div class="content-box">
-    <ul class="poet">
-        <li class="blue"><span class="front">青</span>春的花开花谢让我疲惫却不后悔，</li>
-        <li class="light">四季的雨飞雪飞让我心醉却不堪憔悴。</li>
-        <li class="blue">轻轻的风青青的梦，轻轻的晨晨昏昏，</li>
-        <li class="light">淡淡的云淡淡的泪，淡淡的年年岁岁。</li>
-        <li class="blue">带着点流浪的喜悦我就这样一去不回，</li>
-        <li class="light">没有谁暗示年少的我那想家的枯涩滋味。</li>
-    </ul>
-    <a href="./main.php" class="enter">点击浏览网站内容!</a>
+            <img src="./extra/data.png" alt=""/>
+        </div>
+
+    </div>
+
+    <hr>
 
     <div class="footer">
-        <p>&copy;2011 - 2013 Soulteary. All rights reserved.</p>
-        <p><a href="http://soulteary.com">soulteary.com</a></p>
+        <p>&copy; Company 2013</p>
     </div>
-</div>
-<script type="text/javascript">
-    (function () {
-        var titleScroll = function () {
-            var index = 0;
-            var text = "欢迎光临Story校园相册！";
-            var len = text.length;
-            var init = function () {
-                document.title = text.substring(0, index + 1);
-                index++;
-                if (index >= len) {
-                    index = 0;
-                    document.title = text;
-                }
-                setTimeout(arguments.callee, 150);
-            }
-            init();
-        }
-        titleScroll();
 
-        var timer;
-        var hasLoaded = false;
-
-        var PageLoading = function () {
-            var index = 0;
-            var scroll = document.getElementById('finish');
-            var init = function () {
-                if (index > 95) {
-                    index = 0;
-                }
-                index++;
-                scroll.style.left = index + 'px';
-                if (!hasLoaded) {
-                    timer = setTimeout(arguments.callee, 10);
-                }
-            }
-            init();
-        }
-        PageLoading();
-
-        window.PageLoaded = function () {
-            clearInterval(timer);
-            var loader = document.getElementById('loader');
-            loader.parentNode.removeChild(loader);
-        }
-    })("http://soulteary.com")
-</script>
+</div> <!-- /container -->
 </body>
 </html>
